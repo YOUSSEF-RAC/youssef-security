02411ac195d0e34f7fa43cfbd4cbbf6e2ac5b55a
YOUSSEF-1077513 <1077513@hr.nl>
2024-10-06 23:11:02 +0200

    opdracht 6-10-2024

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 import json
 import os
+import werkzeug.security
 
 app = Flask(__name__, static_url_path='', static_folder='.')
-
 
 students = []
 students_file_path = 'students.json'
@@ -13,6 +13,13 @@ if os.path.exists(students_file_path):
 else:
     print(f"Error: '{students_file_path}' not found or cannot be loaded.")
 
+teachers = []  
+teachers_file_path = 'teachers.json'  
+if os.path.exists(teachers_file_path):
+    with open(teachers_file_path) as f:
+        teachers = json.load(f)
+else:
+    print(f"Error: '{teachers_file_path}' not found or cannot be loaded.")
 
 statements = []
 statements_file_path = 'actiontype_statements.json'
@@ -30,12 +37,51 @@ def login():
 def login_student():
     username = request.form['username']
     password = request.form['password']
-    
 
     for student in students:
-        if student['student_name'] == username and str(student['student_number']) == password:
-            return redirect(url_for('statements'))  
-    
+        if student['student_name'] == username:
+            if 'salt' not in student or 'password' not in student:
+                salt = os.urandom(16).hex()
+                hashed_password = werkzeug.security.generate_password_hash(password + salt)
+
+                student['salt'] = salt
+                student['password'] = hashed_password
+
+                print(f"Nieuwe salt: {salt}, Nieuwe hashed_password: {hashed_password}")  
+
+                with open(students_file_path, 'w') as f:
+                    json.dump(students, f)
+
+            salt = student['salt']
+            if werkzeug.security.check_password_hash(student['password'], password + salt):
+                return redirect(url_for('statements'))
+
+    return redirect(url_for('login'))
+
+@app.route('/login_teacher', methods=['POST'])
+def login_teacher():
+    username = request.form['username']
+    password = request.form['password']
+
+    for teacher in teachers:
+        if teacher['teacher_name'] == username:
+            if 'salt' not in teacher or 'password' not in teacher:
+                salt = os.urandom(16).hex()
+                hashed_password = werkzeug.security.generate_password_hash(password + salt)
+
+
+                teacher['salt'] = salt
+                teacher['password'] = hashed_password
+                
+                print(f"Nieuwe salt: {salt}, Nieuwe hashed_password: {hashed_password}")  
+
+                with open(teachers_file_path, 'w') as f:
+                    json.dump(teachers, f)
+
+            salt = teacher['salt']
+            if werkzeug.security.check_password_hash(teacher['password'], password + salt):
+                return redirect(url_for('statements'))
+
     return redirect(url_for('login'))
 
 @app.route('/statements')
@@ -44,9 +90,7 @@ def statements():
 
 @app.route('/update_result', methods=['POST'])
 def update_result():
-
     result = request.form['result']
-    
     return {'result': 'Success'}
 
 if __name__ == '__main__':
